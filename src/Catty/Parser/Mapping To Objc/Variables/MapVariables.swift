a92e50b61a67a0d31d3fd25017cc9cde98cbe240
GatCode <gatcode@gmail.com>
2019-07-07 07:44:52 +0200

    Resolve all ignored ObjectVariables

--- src/Catty/Parser/Mapping To Objc/Variables/MapVariables.swift
+++ src/Catty/Parser/Mapping To Objc/Variables/MapVariables.swift
@@ -103,6 +103,21 @@ extension CBXMLMapping {
                             }
                         }
 
+                        // also search for unlisted userVariables/Lists
+                        if let scriptList = referencedObject?.scriptList {
+                            for script in scriptList {
+                                if let script = script as? Script, let brickList = script.brickList {
+                                    for brick in brickList {
+                                        if let brick = brick as? Brick, let uVar = brick.uVar {
+                                            if referencedList.contains(uVar) == false {
+                                                referencedList.append(uVar)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
                         result.setObject(NSArray(array: referencedList), forKey: referencedObject)
                     }
                 }
@@ -132,6 +147,21 @@ extension CBXMLMapping {
                                 }
                             } else if let value = list.value {
                                 referencedList.append(allocUserVariable(name: value, isList: false))
+                            }
+                        }
+
+                        // also search for unlisted userVariables/Lists
+                        if let scriptList = referencedObject?.scriptList {
+                            for script in scriptList {
+                                if let script = script as? Script, let brickList = script.brickList {
+                                    for brick in brickList {
+                                        if let brick = brick as? Brick, let uVar = brick.uVar {
+                                            if referencedList.contains(uVar) == false {
+                                                referencedList.append(uVar)
+                                            }
+                                        }
+                                    }
+                                }
                             }
                         }
 
