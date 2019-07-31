0fc83be0454e016818a18d6572b6c20e83886b7a
GatCode <gatcode@gmail.com>
2019-07-31 07:29:47 +0200

    Resolve Recursion Fixme

--- src/Catty/DataModel/Formula/FormulaElement.m
+++ src/Catty/DataModel/Formula/FormulaElement.m
@@ -308,9 +308,6 @@
         return NO;
     if((self.parent != nil && formulaElement.parent == nil) || (self.parent == nil && formulaElement.parent != nil))
         return NO;
-    // FIXME: this leads to an endless recursion bug!!!
-    //    if(self.parent != nil && ![self.parent isEqualToFormulaElement:formulaElement.parent])
-    //        return NO;
     if ((self.parent && (! formulaElement.parent)) || ((! self.parent) && formulaElement.parent))
         return NO;
     
