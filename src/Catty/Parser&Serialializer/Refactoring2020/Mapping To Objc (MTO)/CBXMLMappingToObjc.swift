214c2ccac5b68fb905744eec48f454416444d28b
GatCode <gatcode@wdw.one>
2020-04-14 13:14:21 +0200

    Implement Basic Header

---
+++ src/Catty/Parser&Serialializer/Refactoring2020/Mapping To Objc (MTO)/CBXMLMappingToObjc.swift
@@ -0,0 +1,48 @@
+/**
+ *  Copyright (C) 2010-2020 The Catrobat Team
+ *  (http://developer.catrobat.org/credits)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  (http://developer.catrobat.org/license_additional_term)
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+enum CBXMLMappingToObjc {
+
+    static var mappingSoundList = [Sound]()
+    static var mappingVariableListGlobal = [UserVariable]()
+    static var mappingVariableListLocal = [UserVariable]()
+    static var unsupportedElements = [String]()
+    static var spriteObjectList = [SpriteObject]()
+    static var abortMapping = false
+
+    static func mapCBProjectToProject(project: CBProject?) -> Project? {
+
+        var mappedProject = Project()
+        CBXMLMappingToObjc.mappingSoundList.removeAll()
+        CBXMLMappingToObjc.mappingVariableListGlobal.removeAll()
+        CBXMLMappingToObjc.mappingVariableListLocal.removeAll()
+        CBXMLMappingToObjc.unsupportedElements.removeAll()
+        CBXMLMappingToObjc.spriteObjectList.removeAll()
+        CBXMLMappingToObjc.abortMapping = false
+
+        if let mappedHeader = mapHeader(project: project) {
+            mappedProject.header = mappedHeader
+        }
+
+        return CBXMLMappingToObjc.abortMapping ? nil : mappedProject
+    }
+}
